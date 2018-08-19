d8d7a7ce5f3d7ceae303c50f85684ead7618e003
Sleepy Boyy <timo.kraenzle.2003@gmail.com>
2018-08-19 12:49:06 +0200

    Updated and fixed eval command

--- Xenon/Xenon/Modules/BotOwnerModule.cs
+++ Xenon/Xenon/Modules/BotOwnerModule.cs
@@ -1,10 +1,14 @@
 ﻿#region
 
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Reflection;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using Discord;
 using Discord.Commands;
+using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp.Scripting;
 using Microsoft.CodeAnalysis.Scripting;
 using Xenon.Core;
@@ -18,33 +22,48 @@ namespace Xenon.Modules
     [CommandCategory(CommandCategory.BotOwner)]
     public class BotOwnerModule : CommandBase
     {
+        private readonly IEnumerable<string> _dependencys = new[] {"Discord", "Discord.Net", "Discord.Commands", "Discord.WebSocket", "System", "System.Linq", "System.Collections.Generic", "System.Text", "System.Threading.Tasks"};
+
         [Command("eval")]
         [Summary("Evaluates some code and returns the result")]
         public async Task EvaluateAsync([Remainder] string code)
         {
-            code =
-                $"{Regex.Match(code, PublicVariables.CodeBlockRegex, RegexOptions.Compiled | RegexOptions.Multiline).Groups[2]}";
+            if (Regex.IsMatch(code, PublicVariables.CodeBlockRegex, RegexOptions.Compiled | RegexOptions.Multiline))
+            {
+                code =
+                    $"{Regex.Match(code, PublicVariables.CodeBlockRegex, RegexOptions.Compiled | RegexOptions.Multiline).Groups[2]}";
+            }
+            var assemblys = Assembly.GetEntryAssembly().GetReferencedAssemblies().Select(Assembly.Load).ToList();
+            assemblys.Add(Assembly.GetEntryAssembly());
+            var scriptOptions = ScriptOptions.Default
+                .WithReferences(assemblys.Select(x => MetadataReference.CreateFromFile(x.Location)))
+                .WithImports(Assembly.GetEntryAssembly().GetTypes().Select(x => x.Namespace).Distinct());
+            var embed = NormalizeEmbed("Evaluate Code", "Debugging...");
+            var message = await ReplyAsync(embed: embed.Build());
             try
             {
-                var script = CSharpScript.Create<object>(code,
-                    ScriptOptions.Default
-                        .WithImports("System.Math")
-                        .AddReferences(Assembly.GetCallingAssembly(), Assembly.GetEntryAssembly(),
-                            Assembly.GetExecutingAssembly()), typeof(EvaluateObject));
-                script.Compile();
-                var result = await script.RunAsync(new EvaluateObject
+                var result = await CSharpScript.EvaluateAsync($"{string.Join("\n", _dependencys.Select(x => $"using {x};"))}\n{code}", scriptOptions, new EvaluateObject
                 {
                     Client = Context.Client,
                     Context = Context,
                     Database = Database,
                     Random = Random,
                     Server = Server
-                });
-                await ReplyAsync($"{result.ReturnValue}");
+                }, typeof(EvaluateObject));
+                embed.WithTitle("Completed")
+                    .WithDescription($"Result: {result ?? "none"}");
+                await message.ModifyAsync(x => x.Embed = embed.Build());
             }
             catch (Exception e)
             {
-                await ReplyAsync(e.Message ?? e.InnerException.Message);
+                embed.WithTitle("Failure")
+                    .WithDescription($"Reason: {e.Message ?? e.InnerException.Message}");
+                await message.ModifyAsync(x => x.Embed = embed.Build());
+            }
+            finally
+            {
+                GC.Collect();
+                GC.WaitForPendingFinalizers();
             }
         }
     }
